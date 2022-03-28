a70a6be838f068d11362e258a426ef737ee13690
oowtl <oowtl7425@gmail.com>
2022-03-28 23:12:54 +0900

    20220328 | done | navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // 기능
+import axios from 'axios';
 import {Routes, Route, Link} from 'react-router-dom';
+import { useState } from 'react';
 // components
 import MainPage from './components/MainPage';
 import Navbar from './components/navbar';
@@ -8,9 +10,40 @@ import styles from './App.module.css';
 
 
 function App() {
+  // state
+  const [data, setData] = useState([]);
+
+  //function
+  const youtubeSearch = (type, word) => {
+    // type : init (처음 시작할 때) , search ( 검색할 때)
+    const searchParams = {
+      params: {
+        key: `${process.env.REACT_APP_API_KEY}`,
+        part: 'snippet',
+        maxResults: 25,
+        q: word,
+        order: 'relevance'
+      }
+    }
+    if (type === "search") {
+      searchParams.order = 'viewCount';
+    }
+    axios.get(`${process.env.REACT_APP_API_URL}/search`,searchParams)
+      .then( res => {
+        setData(res.data.items);
+      })
+      .catch( err => {
+        console.log(err);
+      })
+  };
+
+  const handleSearchClick = (word) => {
+    youtubeSearch("search", word);
+  }
+
   return (
     <div className={styles.App}>
-      <Navbar />
+      <Navbar handleSearchClick={handleSearchClick}/>
       <Routes>
         <Route path='/' element={<MainPage />} />
         {/* <Route> */}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 // css
 import styles from './navbar.module.css'
 
 const Navbar = (props) => {
+  const searchRef = useRef();
+
+  const onSearchClick = (event) => {
+    event.preventDefault();
+    if (searchRef.current.value !== '') {
+      props.handleSearchClick(searchRef.current.value);
+    }
+    searchRef.current.value = '';
+  };
+
   return (
     <nav className={styles.nav}>
       <img 
@@ -11,8 +21,8 @@ const Navbar = (props) => {
         src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="youtube logo" />
       <span>Youtube</span>
       <div className={styles.search}>
-        <input type="text" placeholder='Search...' />
-        <button>
+        <input ref={searchRef} type="text" placeholder='Search...' />
+        <button onClick={onSearchClick}>
           <img
             src={`${process.env.PUBLIC_URL}/images/search.png`} alt="search button" />
         </button>
